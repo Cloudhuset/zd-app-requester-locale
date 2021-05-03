4a5d6093feec5303dbfaa7f682aed0f7ca03acb0
Jens Ahlsten Herlevsen <jensherlevsen@gmail.com>
2021-05-03 14:23:53 +0200

    refactor to use functional components

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect, useCo } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import zafClient from '../misc/ZAFClient'
 
@@ -11,28 +11,10 @@ import {
   setAppHeight
 } from '../actions'
 
-@connect(
-  state => ({
-    requester: state.requester,
-    locales: state.locales,
-    isLoading: state.isLoadingRequester && state.isLoadingLocales,
-    isSettingLocale: state.isSettingLocale
-  }),
-  dispatch => ({
-    ...bindActionCreators({
-      getRequester,
-      setRequesterLocale,
-      getLocales,
-      resetStore
-    }, dispatch),
-  })
-)
-export default class Main extends React.Component {
+const Main = props => {
+  const { requester, locales, isLoading, isSettingLocale, setRequesterLocale } = props
 
-  constructor(props) {
-    super(props)
-
-    props.resetStore()
+  React.useEffect(() => {
     Promise.all([
       props.getRequester(),
       props.getLocales()
@@ -51,61 +33,69 @@ export default class Main extends React.Component {
         }, 100);
       })
     })
-  }
+  }, [])
 
-  render() {
-    const { requester, locales, isLoading, isSettingLocale } = this.props
-
-    if (isLoading || !locales) {
-      return (
-        <div>
-          Loading, please wait...
-        </div>
-      )
-    }
-
-    if (!requester) {
-      return (
-        <div style={{ paddingTop: '10px', textAlign: 'center' }}>
-          Please add a requester to the ticket
-          <Footer />
-        </div>
-      )
-    }
-
-    const localeArr = locales.filter(loc => loc.locale === requester.locale)
-
-    return <div style={{ overflow: 'hidden' }}>
-      <select
-        onChange={this.handleLanguageChange.bind(this)}
-        value={(requester && localeArr.length === 1) && localeArr[0].id}
-        style={{ width: '100%', height: '30px', marginTop: '10px' }}
-      >
-        {locales.map(locale => {
-          return <option key={locale.id} value={locale.id}>
-            {locale.name}
-          </option>
-        })}
-      </select>
-      {isSettingLocale &&
-        <div style={{ width: '25px', height: '25px', position: 'absolute', bottom: 0, right: 0 }}>
-          <svg className="spinner" viewBox="0 0 50 50">
-            <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
-          </svg>
-        </div>}
-      <Footer />
-    </div>
-  }
-
-  handleLanguageChange(e) {
-    const { setRequesterLocale, requester } = this.props;
-
+  const handleLanguageChange = e => {
     const value = e.target.value;
     setRequesterLocale(requester.id, value).then(() => {
-      this.props.getRequester(requester.id)
+      getRequester(requester.id)
     });
   }
+
+  if (isLoading || !locales) {
+    return <div>Loading, please wait...</div>
+  }
+
+  if (!requester) {
+    return (
+      <div style={{ paddingTop: '10px', textAlign: 'center' }}>
+        Please add a requester to the ticket
+        <Footer />
+      </div>
+    )
+  }
+
+  const localeArr = locales.filter(loc => loc.locale === requester.locale)
+
+  return <div style={{ overflow: 'hidden' }}>
+    <select
+      onChange={handleLanguageChange}
+      value={(requester && localeArr.length === 1) && localeArr[0].id}
+      style={{ width: '100%', height: '30px', marginTop: '10px' }}
+    >
+      {locales.map(locale => {
+        return <option key={locale.id} value={locale.id}>
+          {locale.name}
+        </option>
+      })}
+    </select>
+    {isSettingLocale &&
+      <div style={{ width: '25px', height: '25px', position: 'absolute', bottom: 0, right: 0 }}>
+        <svg className="spinner" viewBox="0 0 50 50">
+          <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+        </svg>
+      </div>}
+    <Footer />
+  </div>
 }
+
+const mapStateToProps = state => ({
+  requester: state.requester,
+  locales: state.locales,
+  isLoading: state.isLoadingRequester && state.isLoadingLocales,
+  isSettingLocale: state.isSettingLocale
+})
+
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators({
+    getRequester,
+    setRequesterLocale,
+    getLocales,
+    resetStore
+  }, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main)
 
 const Footer = () => (
   <footer>
