a03e922b8dc7b20c9a7df3d767cee3164145cdfe
Jens Ahlsten Herlevsen <jensherlevsen@gmail.com>
2021-05-03 14:24:20 +0200

    Remove unused import

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect, useCo } from 'react-redux'
+import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import zafClient from '../misc/ZAFClient'
 
